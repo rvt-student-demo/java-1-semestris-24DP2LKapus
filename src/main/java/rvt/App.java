b07d4c5cea04601a05db3569c7b8365213330fb8
24DP2LKapus <a242772lk@rvt.lv>
2025-09-09 10:31:43 +0000

    Message

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -1,7 +1,19 @@
 package rvt;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        // Your code goes here...
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What's your name?");
+
+        String name = scanner.nextLine();
+
+        System.out.println("Hi " + name);
+
+        // Write your program here
+        
     }
 }
