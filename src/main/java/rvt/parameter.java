63d9b0e398c17156ac959c68d416b9f60f7ee3d9
24DP2LKapus <a242772lk@rvt.lv>
2025-10-07 07:55:01 +0000

    From parameter to one

---
+++ src/main/java/rvt/parameter.java
@@ -0,0 +1,18 @@
+package rvt;
+
+import java.util.Scanner; 
+
+public class parameter {
+    public static void main(String[] args) {
+        printFromNumberToOne(5);
+
+    }
+    public static void printFromNumberToOne(int number) {
+         for (int i = number; i >= 1; i--) {
+            System.out.println(i);
+        }
+             
+            
+    }
+}
+ 
