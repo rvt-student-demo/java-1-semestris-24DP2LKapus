bc77d954d2e641d3bd57f264f7a45ad0b6574829
Laura Kapustina <a242772lk@rvt.lv>
2025-11-28 13:08:40 +0200

    Divisible by three

---
+++ src/main/java/rvt/DivisiblenbyThree.java
@@ -0,0 +1,18 @@
+package rvt;
+
+import java.util.Scanner;
+
+public class DivisiblenbyThree {
+    public static void main(String[] args) {
+        divisibleByThreeInRange(3, 6); }
+
+    public static void divisibleByThreeInRange(int start, int end) {
+            for (int i = start; i <= end; i++){
+                if (i % 3 == 0) {
+                    System.out.println(i);
+                }
+            }
+        }
+    
+    
+}
